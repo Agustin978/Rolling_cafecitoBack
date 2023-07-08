3d9049aef78d771b64010ce8ee740a12d43706ee
Agustin978 <loboagus10@gmail.com>
2023-07-08 02:22:01 -0300

    Agregado el puerto que se usara

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-import { Express } from "express";
-console.log('Hola Mundo!!! 1');
+import express from "express";
+//Debo configurar un puerto
+const app = express();
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+    console.log("Estoy en el puerto: "+app.get("port"));
+});
