9afb150f4412ba07969d47a79b87001fe0a424aa
Agustin978 <loboagus10@gmail.com>
2023-07-09 01:50:24 -0300

    Creado el modelo para la base de datos.

---
+++ src/models/producto.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+//Un esquema seria como un prototipo.
+const recetaSchema = new Schema({ //En el objeto dentro de scheme debe tener las propiedades del objeto que quiero guardar en la base de datos
+    nombrePlatillo: {
+        type: String,
+        minLength: 2,
+        maxLength: 100,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        minLength:10,
+        maxLength:2000,
+        required: true
+    },
+    imagen: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    }
+}); //El id no se agrega al esquema porque mongus ya crea un id por defecto.
+
+const Receta = model('receta', recetaSchema);
+export default Receta;
