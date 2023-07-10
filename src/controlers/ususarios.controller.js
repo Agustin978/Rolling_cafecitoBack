93e451a4339347734496b724f0f72484f0bacf1d
Agustin978 <loboagus10@gmail.com>
2023-07-10 03:52:43 -0300

    Ingresadas las funciones de obtenerUsuarios (todos) y de crearUsuario

--- src/controlers/ususarios.controller.js
+++ src/controlers/ususarios.controller.js
@@ -17,4 +17,43 @@ export const obtieneUsuario = async (req, res) =>
     }
 }
 
-//Controlador para 
+//Controlador para obtener todos los usuarios
+export const obtenerUsuarios = async (req, res) =>
+{
+    try
+    {
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    }catch(error)
+    {
+        console.log('A ocurrido un error al intentar comunicarse con la base de datos. Info de error: '+error);
+        res.status(400).json({
+            mensaje: 'Error al buscar los usuarios en la base de datos.'
+        });
+    }
+}
+
+export const creaUsuario = async (req, res) => 
+{
+    try
+    {
+        const errors = validationResult(req);
+        if(!errors.isEmpty())
+        {
+            return res.status(400).json({
+                error: errors.array()
+            });
+        }
+        const usuarioNuevo = new Usuario(req.body);
+        await usuarioNuevo.save();
+        res.status(200).json({
+            mensaje: 'El nuevo usuario fue almacenado exitosamente.'
+        })
+    }catch(error)
+    {
+        console.log('A ocurrido un error al intentar comunicarse con la base de datos. Info de error: '+error);
+        res.status(400).json({
+            mensaje: 'Error al intentar ingresar el nuevo usuario en la base de datos.'
+        });
+    }
+}
