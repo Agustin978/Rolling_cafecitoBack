3bb253a9baa77243b231d5ee46d1fe690f738485
Agustin978 <loboagus10@gmail.com>
2023-07-12 09:54:58 -0300

    Cambie la direccion de la base de datos en config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 import 'dotenv/config';
-export const MOGODB_URI = process.env.MOGODB_URI || 'mongodb://localhost:27017/recetasDB'; //Creo la base de datos de manera local
+export const MOGODB_URI = process.env.MOGODB_URI || 'mongodb://127.0.0.1:27017/recetasDB'; //Creo la base de datos de manera local
